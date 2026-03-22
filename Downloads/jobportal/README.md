c2d38c0f1d28df8369f7e6949f317c5a398ed81b
RAHUL CH <rahulcch794@gmail.com>
2026-03-22 23:50:48 +0530

    Delete Downloads/jobportal directory

--- Downloads/jobportal/README.md
+++
@@ -1,1 +0,0 @@
-# AI_Job-Portal-Backend
--- Downloads/jobportal/src/main/java/com/rahul/jobportal/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rahul.jobportal.repository;
-
-import com.rahul.jobportal.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
