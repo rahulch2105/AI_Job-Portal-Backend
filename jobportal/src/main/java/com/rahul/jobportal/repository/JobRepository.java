dc1dd2e7d7f169dce2a718a56625b9badbe77fbe
RAHUL CH <rahulcch794@gmail.com>
2026-03-22 23:51:37 +0530

    Delete jobportal directory

--- jobportal/src/main/java/com/rahul/jobportal/repository/JobRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rahul.jobportal.repository;
-
-import com.rahul.jobportal.entity.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<Job, Long> {
-
-}
